ddc5b119596049ab98bfeb294c4864f607056d19
huluoyang <huluoyang@gmail.com>
2016-07-05 22:00:34 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/index.html', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/service.html', function(req, res, next) {
+  res.render('service', { title: 'Express' });
+});
+router.get('/about.html', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+module.exports = router;
